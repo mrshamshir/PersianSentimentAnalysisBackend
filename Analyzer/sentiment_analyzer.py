98406de4ebf8a318b73c014bc072a7676543aa75
mrshamshir <mr.sh@aut.ac.ir>
2020-09-19 11:23:24 +0430

    3 datasets and 3 ml models added

--- Analyzer/sentiment_analyzer.py
+++ Analyzer/sentiment_analyzer.py
@@ -9,6 +9,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import f1_score
 
 # Preprocessing
 ## from StopWords_persian import stopwords_output
@@ -16,16 +17,24 @@ from sklearn.pipeline import Pipeline
 from hazm import *
 
 # Import & Analyze Dataset
+test = pd.read_csv('Dataset/test.csv', index_col=None, header=None, encoding="utf-8")
 
+x_test = test[0]
+y_test = test[1]
+
+x_test = np.asarray(x_test)
+y_test = np.asarray(y_test)
+
+original = pd.read_csv('Dataset/original.csv', index_col=None, header=None, encoding="utf-8")
+balanced = pd.read_csv('Dataset/balanced.csv', index_col=None, header=None, encoding="utf-8")
 translation = pd.read_csv('dataset/translation.csv', index_col=None, header=None, encoding="utf-8")
 
-selected_dataset = translation
+selected_dataset = balanced
 
 selected_dataset = selected_dataset.sample(frac=1).reset_index(drop=True)
 
 x_train = selected_dataset[0]
 y_train = selected_dataset[1]
-print(y_train)
 
 # Convert dataframes to numpy arrays
 x_train = np.asarray(x_train)
@@ -62,15 +71,17 @@ train_docs = np.empty_like(x_train)
 for index, document in enumerate(x_train):
     train_docs[index] = clean_doc(document)
 
-# Machine Learning Algorithms
-
+# Applying preprocessing step to test data
+test_docs = np.empty_like(x_test)
+for index, document in enumerate(x_test):
+    test_docs[index] = clean_doc(document)
 
 # Make stop word set
 
-
 file = pd.read_csv('dataset/per_sw.csv', sep="\n", encoding="utf-8")
-numpy_array = file.values
-stop_set = set(numpy_array.flatten())
+stop_set = set(file.values.flatten())
+
+# Machine Learning Algorithms
 
 # When building the vocabulary ignore terms that have a document frequency strictly lower than
 # the given threshold. This value is also called cut-off in the literature.
@@ -91,13 +102,64 @@ naive_bayes = Pipeline([('vect', CountVectorizer(tokenizer=tokenize,
                         ('tfidf', TfidfTransformer(sublinear_tf=True)),
                         ('clf', MultinomialNB())])
 naive_bayes = naive_bayes.fit(x_train, y_train)
-x = 'حساسیت لمسی خود نمایشگر هم کاملا عالیست.'
-y = 'سرعت اجرا بسيار بالا است و مصرف باتري نيز مناسب است.'
-z = 'فارسيش هم عاليه اين گوشي تازه اومده.'
-w = 'این به این معناست که از صفحه نمایش آن نمی‌توان انتظار تصویر چندان خوبی در مقابل صفحه نمایش‌های Super AMOLED و یا Super Clear LCD را داشت.'
-temp = naive_bayes.predict_proba([y, z, w])
-temp2 = naive_bayes.predict_log_proba([y, z, w])
+naive_score = naive_bayes.score(x_test, y_test)
+print('Naive Bayes Model: ', naive_score)
+predict_nb = naive_bayes.predict(x_test)
 
-print(temp)
-print(naive_bayes.predict([y, z, w]))
-print(temp2)
+# test and get result table for one single data for NB
+# x = 'حساسیت لمسی خود نمایشگر هم کاملا عالیست.'
+# y = 'سرعت اجرا بسيار بالا است و مصرف باتري نيز مناسب است.'
+# z = 'فارسيش هم عاليه اين گوشي تازه اومده.'
+# w = 'این به این معناست که از صفحه نمایش آن نمی‌توان انتظار تصویر چندان خوبی در مقابل صفحه نمایش‌های Super AMOLED و یا Super Clear LCD را داشت.'
+# temp = naive_bayes.predict_proba([y, z, w])
+#
+# print(naive_bayes.classes_)
+# print(naive_bayes.predict([y, z, w]))
+# print(temp)
+
+
+## Support Vector Machine
+
+
+# Linear Support Vector Machine Model
+svm = Pipeline([('vect', CountVectorizer(tokenizer=tokenize,
+                                         analyzer='word', ngram_range=(1, 2),
+                                         min_df=min_df, lowercase=False)),
+                ('tfidf', TfidfTransformer(sublinear_tf=True)),
+                ('clf-svm', LinearSVC(loss='hinge', penalty='l2',
+                                      max_iter=1000))])
+
+svm = svm.fit(x_train, y_train)
+linear_svc_score = svm.score(x_test, y_test)
+print('Linear SVC Model: ', linear_svc_score)
+predict_svm = svm.predict(x_test)
+
+# test and get result table for one single data for SVM
+
+# x = svm.decision_function([x_test[0]])
+# y = svm.predict([x_test[0]])
+# print(x)
+# print(y)
+
+## Stochastic Gradient Descent
+
+
+# SGD (Stochastic Gradient Descent) Model
+sgd = Pipeline([('vect', CountVectorizer(tokenizer=tokenize,
+                                         analyzer='word', ngram_range=(1, 2), min_df=min_df, lowercase=False)),
+                ('tfidf', TfidfTransformer(sublinear_tf=True)),
+                ('clf-svm', SGDClassifier(loss='hinge', penalty='l2',
+                                          alpha=1e-3, max_iter=1000))])
+sgd = sgd.fit(x_train, y_train)
+sgd_score = sgd.score(x_test, y_test)
+print('SGD Model: ', sgd_score)
+predict_sgd = sgd.predict(x_test)
+
+f1_NB = f1_score(y_test, predict_nb, average='weighted')
+print("F1 score of NB model:" + str(f1_NB))
+
+f1_SVM = f1_score(y_test, predict_svm, average='weighted')
+print("F1 score of SVM model:" + str(f1_SVM))
+
+f1_SGD = f1_score(y_test, predict_sgd, average='weighted')
+print("F1 score of SGD model:" + str(f1_SGD))
